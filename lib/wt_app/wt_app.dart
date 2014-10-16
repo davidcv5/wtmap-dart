d1f2fbfaaaf788f64dcc2d33cfbde3c034b3d7f7
David Cruz <davidcv5@gmail.com>
2014-10-16 02:01:26 -0400

    Adding router element and binding to the route attribute

--- lib/wt_app/wt_app.dart
+++ lib/wt_app/wt_app.dart
@@ -5,5 +5,15 @@ import 'package:polymer/polymer.dart';
 class WtApp extends PolymerElement {
   WtApp.created() : super.created();
   
+  @published String test;
+  @published String route;
+  
+  testChanged(oldValue){
+    print('$test was $oldValue with route: $route');
+  }
+  
+  routeChanged(oldValue){
+    print('WtApp.route changed from [$oldValue] to [$route]');
+  }
 }
 
--- web/main.dart
+++ web/main.dart
@@ -1,9 +1,28 @@
 
 import 'package:polymer/polymer.dart';
+import 'package:route_hierarchical/client.dart';
 
 void main() {
   // Init polymer.
   initPolymer();
+//  var router = new Router(useFragment: true);
+//    router.root
+//      ..addRoute(name: 'article', path: '/article/:articleId', enter: showArticle)
+//      ..addRoute(name: 'home', defaultRoute: true, path: '/', enter: showHome);
+//    router.listen();
+    
+}
+
+void showHome(RouteEvent e) {
+  // nothing to parse from path, since there are no groups
+  print('this is home');
+}
+
+void showArticle(RouteEvent e) {
+  var articleId = e.parameters['articleId'];
+  // show article page with loading indicator
+  // load article from server, then render article
+  print('article: $articleId');
   
 }
 
